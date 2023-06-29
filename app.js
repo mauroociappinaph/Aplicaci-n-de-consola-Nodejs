4fd3ee27d9b75e582a0fd47991f3c2668ba6c633
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-29 12:41:33 -0300

    stdin - stdout - Readline

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require("colors");
+
+const { mostrarMenu , pausa} = require("./helpers/mensajes");
 console.clear()
 
 const main = async () => {
   console.log("hello");
+
+  mostrarMenu();
+
+  //pausa();
 };
 
 main();
