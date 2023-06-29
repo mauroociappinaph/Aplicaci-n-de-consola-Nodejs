d2310fde63efabe39eb5b8bb5b153ae14a3afe4b
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-29 13:00:01 -0300

    Funcionalidad de menu Infinito

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("colors");
 
-const { mostrarMenu , pausa} = require("./helpers/mensajes");
-console.clear()
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
+console.clear();
 
 const main = async () => {
   console.log("hello");
+  let opt = "";
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+
+    if (opt !== "0") await pausa();
+  } while (opt !== "0");
 
   mostrarMenu();
 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,37 +1,43 @@
 require("colors");
 
-const pausa = () => {
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const mostrarMenu = () => {
+  return new Promise((resolve) => {
+    console.clear();
+    console.log("====================================".green);
+    console.log("      Seleccione una opción".white);
+    console.log("====================================\n".green);
 
-  readline.question(`\nPresione ${"ENTER".blue} para continuar\n`, (opt) => {
-    readline.close();
+    console.log(`${"1".green} - Agregar tarea`);
+    console.log(`${"2".green} - Listar tareas`);
+    console.log(`${"3".green} - Listar tareas completadas`);
+    console.log(`${"4".green} - Listar tareas pendientes`);
+    console.log(`${"5".green} - Completar tarea(s)`);
+    console.log(`${"6".green} - Borrar tarea`);
+    console.log(`${"0".green} - Salir\n`);
+
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question("Seleccione una opción: ", (opt) => {
+      readline.close();
+      resolve(opt);
+    });
   });
 };
 
-const mostrarMenu = () => {
-  console.clear();
-  console.log("====================================".green);
-  console.log("      Seleccione una opción".white);
-  console.log("====================================\n".green);
+const pausa = () => {
+  return new Promise((resolve) => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  console.log(`${"1".green} - Agregar tarea`);
-  console.log(`${"2".green} - Listar tareas`);
-  console.log(`${"3".green} - Listar tareas completadas`);
-  console.log(`${"4".green} - Listar tareas pendientes`);
-  console.log(`${"5".green} - Completar tarea(s)`);
-  console.log(`${"6".green} - Borrar tarea`);
-  console.log(`${"0".green} - Salir\n`);
-
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  readline.question("Seleccione una opción: ", (opt) => {
-    readline.close();
+    readline.question(`\nPresione ${"ENTER".blue} para continuar\n`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
 
